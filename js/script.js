6a7d4ca688b177886809b2df664999cd161b1b0d
Egor <egorr316@gmail.com>
2025-01-13 10:51:05 +0200

    Доданi коментарi  до коду

--- js/script.js
+++ js/script.js
@@ -5,16 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const nextButton = document.querySelector('.reviews__next');
     const burger = document.querySelector(".burger");
 
+    //Обробник подiй для бургер-мени
     burger.addEventListener('click', (event) =>{
        event.currentTarget.classList.toggle("burger--active");
        document.querySelector(".header__menu").classList.toggle("header__menu--active");
     });
 
+    // Створюю слайдер
     let currentIndex = 0;
     function updateSliderPosition() {
         slider.style.transform = `translateX(-${currentIndex * 100}%)`;
     }
-
+    
+    //Обробник подiй для кнопки назад для слайдера
     prevButton.addEventListener('click', () => {
         if (currentIndex > 0) {
             currentIndex--;
@@ -24,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
         updateSliderPosition();
     });
 
+    //Обробник подiй для кнопки вперед для слайдера
     nextButton.addEventListener('click', () => {
         if (currentIndex < reviews.length - 1) {
             currentIndex++;
